966962a56e9766166d79e330ebad42853a24a0c9
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-11-01 09:13:04 +0800

    Add files via upload

---
+++ C++PrimerPlus/2021.2学习C++/生成DLL文件/pch.cpp
@@ -0,0 +1,25 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int minus(int a, int b)
+{
+	return a - b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+double divide(int a, int b)
+{
+	double m = (double)a / b;
+	return m;
+}
---
+++ C++PrimerPlus/2021.2学习C++/生成DLL文件/pch.h
@@ -0,0 +1,24 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+
+#endif //PCH_H
+
+//定义宏
+#ifdef IMPORT_DLL
+#else
+#define IMPORT_DLL extern "C" _declspec(dllimport) //指的是允许将其给外部调用
+#endif
+//声明有哪些函数
+extern "C" _declspec(dllexport) int add(int a, int b);
+extern "C" _declspec(dllexport) int minus(int a, int b);
+extern "C" _declspec(dllexport) int multiply(int a, int b);
+extern "C" _declspec(dllexport) double divide(int a, int b);
