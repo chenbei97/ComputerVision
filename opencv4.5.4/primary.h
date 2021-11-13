1a5e369ad15197c470e419ee692a184c842d8efb
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-11-13 18:34:59 +0800

    Update primary.h

--- opencv4.5.4/primary.h
+++ opencv4.5.4/primary.h
@@ -1,8 +1,10 @@
 #include <opencv2/xfeatures2d.hpp>
 #include <opencv2/opencv.hpp>
+const std ::string imgAddr ="C:/Users/chenbei/Documents/Visual Studio 2019/ItemFiles/Opencv4/images/";
 void imread_imshow();
 void image_type_conversion_imwirte();
 void image_assignment_copy_clone();
 void image_pixel_read_write_operation();
 void image_arithmetic_operation();
 void image_mask_operation();
+void image_blend();
