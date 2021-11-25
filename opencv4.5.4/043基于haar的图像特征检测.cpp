d194c1e8db94228e97e7564adeff3a9933b3d3c6
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-11-25 10:30:48 +0800

    Add files via upload

---
+++ opencv4.5.4/043基于haar的图像特征检测.cpp
@@ -0,0 +1,12 @@
+#include "primary.h"
+using namespace cv;
+#include <vector>
+void image__feature_detection_haar()
+{
+    /*
+        高类间变异性、低类内变异性、局部强度差、不同尺度、计算效率高
+        nothing
+    
+    */
+
+}
