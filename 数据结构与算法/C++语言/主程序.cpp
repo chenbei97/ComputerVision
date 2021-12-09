259d4ae3b843374473d7c6354c7d559f484ed99b
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-12-09 12:10:05 +0800

    Update 主程序.cpp

--- 数据结构与算法/C++语言/主程序.cpp
+++ 数据结构与算法/C++语言/主程序.cpp
@@ -7,10 +7,14 @@ int main()
 	//_charter2_test_and_debug();
 	//_charter2_spatial_complexity();
 	//_charter2_time_complexity();
+	_charter2_best_worst_average_operation_count();
 
 	const int n = 10;
 	int a[n] = { 6,5,8,4,3,1,13,0,66 ,-2};
-	//array_sorting_method(a, n ,countSort);
-	array_sorting_method(a, n, selectSort);
+	array_sorting_method(a, n ,countSort);
+	std::cout << std::endl;
+	array_sorting_method(a, n, selectSort, true);
+	std::cout << std::endl;
+	array_sorting_method(a, n, bubbleSort);
 	return 0;
 }
