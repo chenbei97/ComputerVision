5544e34d922690caa31a6ea6f3f66444ad052a7e
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-12-09 12:25:45 +0800

    Add files via upload

---
+++ 数据结构与算法/C++语言/charter2_04最好、最坏和平均操作计数.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+template<class T>
+void insert_ordered_array(T a[], int& n, const T& x);
+void _charter2_best_worst_average_operation_count()
+{
+	const int n = 5;
+	int n1 = n;
+	int a[n+1] = { 1,3,5,7,9 };
+	int x = 0;
+	insert_ordered_array(a, n1, x);
+	std::cout << "n = " << n1 << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d] = %d ", i, a[i]);
+	}
+}
+template<class T>
+void insert_ordered_array(T a[], int& n, const T& x)
+{
+	int i; // 不能在循环里边定义int i=0,因为至少执行一次a[i+1]=x
+	for (i = n - 1; i >= 0 && x < a[i]; i--)
+		a[i + 1] = a[i]; // 虽然i没有初始化但是执行完for循环后i的值为-1
+	a[i + 1] = x; // 插入开头时i=-1,才执行a[0]=x
+	n++;
+}
