93d16ea5d4762973d602c3b2fe1bd1113a6bad29
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-12-10 09:42:20 +0800

    Update 主程序.cpp

--- 数据结构与算法/C++语言/主程序.cpp
+++ 数据结构与算法/C++语言/主程序.cpp
@@ -9,6 +9,8 @@ int main()
 	//_charter2_time_complexity();
 	_charter2_best_worst_average_operation_count();
 	_charter2_count_sort_situ_rearrangement();
+	_charter2_select_sort_timely_termination();
+	_charter2_bubble_sort_timely_termination();
 
 	const int n = 10;
 	int a[n] = { 6,5,8,4,3,1,13,0,66 ,-2};
