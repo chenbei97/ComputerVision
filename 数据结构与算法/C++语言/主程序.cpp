7a6e02e14fa233b2d30914709792d36c549028ad
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-12-10 21:23:11 +0800

    Update 主程序.cpp

--- 数据结构与算法/C++语言/主程序.cpp
+++ 数据结构与算法/C++语言/主程序.cpp
@@ -7,21 +7,22 @@ int main()
 	//_charter2_test_and_debug();
 	//_charter2_spatial_complexity();
 	//_charter2_time_complexity();
-	_charter2_best_worst_average_operation_count();
-	_charter2_count_sort_situ_rearrangement();
-	_charter2_select_sort_timely_termination();
-	_charter2_bubble_sort_timely_termination();
-	_charter2_insert_sort();
+	//_charter2_best_worst_average_operation_count();
+	//_charter2_count_sort_situ_rearrangement();
+	//_charter2_select_sort_timely_termination();
+	//_charter2_bubble_sort_timely_termination();
+	//_charter2_insert_sort();
+	_charter2_calculation_of_program_steps();
 
-	const int n = 10;
-	int a[n] = { 6,5,8,4,3,1,13,0,66 ,-2};
-	array_sorting_method(a, n ,countSort);
-	std::cout << std::endl;
-	array_sorting_method(a, n, selectSort, true);
-	std::cout << std::endl;
-	array_sorting_method(a, n, bubbleSort);
-        std::cout << std::endl;
-	array_sorting_method(a, n, insertSort,true);
-	std::cout << std::endl;
+	//const int n = 10;
+	//int a[n] = { 6,5,8,4,3,1,13,0,66 ,-2};
+	//array_sorting_method(a, n ,countSort);
+	//std::cout << std::endl;
+	//array_sorting_method(a, n, selectSort, true);
+	//std::cout << std::endl;
+	//array_sorting_method(a, n, bubbleSort);
+        //std::cout << std::endl;
+	//array_sorting_method(a, n, insertSort,true);
+	//std::cout << std::endl;
 	return 0;
 }
