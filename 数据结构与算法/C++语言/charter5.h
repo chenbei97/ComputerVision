737e8f01ff3b5cb65c3c406f304adab7e623095f
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-12-27 14:04:56 +0800

    Update charter5.h

--- 数据结构与算法/C++语言/charter5.h
+++ 数据结构与算法/C++语言/charter5.h
@@ -2,3 +2,5 @@
 void _charter5_changeLength1D();
 void _charter5_concrete_class_instantiation();
 void _charter5_iterator_concept();
+void _charter5_bidirectional_iterator();
+void _charter5_arrayList_with_bidirectionalIterator();
