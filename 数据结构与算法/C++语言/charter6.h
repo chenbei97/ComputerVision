3a04518779dfbf99fd087ec999352ff2680b258a
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2021-12-30 16:24:05 +0800

    Update charter6.h

--- 数据结构与算法/C++语言/charter6.h
+++ 数据结构与算法/C++语言/charter6.h
@@ -4,3 +4,4 @@ void _charter6_chainList_testCode();
 void _charter6_chainListWithIterator_testCode();
 void _charter6_chainListWithIteratorExtend_testCode();
 void _charter6_chainListCircular_testCode();
+void _charter6_StudentRecord1_binSort_testCode();
