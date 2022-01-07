079b4a307eb97a853092049b2be33f86edb21efc
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2022-01-07 10:28:09 +0800

    Update charter7.h

--- 数据结构与算法/C++语言/charter7.h
+++ 数据结构与算法/C++语言/charter7.h
@@ -4,4 +4,5 @@ void _charter7_irregular_array2d();
 void _charter7_matrixAdd();
 void _charter7_matrixMultiply_sameSize();
 void _charter7_matrixMultiply_differentSize();
+void _charter7_matrixTranspose();
 void _charter7_matrix_testCode();
