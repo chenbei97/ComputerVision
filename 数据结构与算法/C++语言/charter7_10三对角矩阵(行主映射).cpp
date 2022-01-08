3bd598db38db867733b2de374fc2ce4c8543e13e
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2022-01-08 16:02:10 +0800

    Add files via upload

---
+++ 数据结构与算法/C++语言/charter7_10三对角矩阵(行主映射).cpp
@@ -0,0 +1,20 @@
+#include "charter7_tridiagonalMatrixRowMapping.h"
+void _charter7_tridiagonalMatrixRowMapping()
+{
+	const int n = 5; // 对角线只有3n-2个元素
+	int a[3*n-2] = {1,2,3,4,5,6,7,8,9,10,11,12,13}; // 行主映射要求按行顺序标注元素
+	tridiagonalMatrixRow<int> A(a, n);
+	cout << "A is " << endl;
+	cout << A << endl;
+	A.set(5, 5, 100);
+	cout << "A(5,5) = " << A.get(5, 5) << endl;
+	try
+	{
+		A.set(5, 3, 20); // throw e
+	}
+	catch (illegalParameterValue e)
+	{
+		e.outputMessage();
+	}
+	system("pause");
+}
