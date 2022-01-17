cfffb7695fb536c659eeb032ef3e884064cde9b1
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2022-01-17 16:43:27 +0800

    Update charter8.h

--- 数据结构与算法/C++语言/charter8.h
+++ 数据结构与算法/C++语言/charter8.h
@@ -3,3 +3,5 @@ void _charter8_derivedArrayStack();
 void _charter8_arrayStack();
 void _charter8_linkedStack();
 void _charter8_parenthesis_matching();
+void _charter8_towerOfHanoi_byRecursion();
+void _charter8_towerOfHanoi_byStack();
