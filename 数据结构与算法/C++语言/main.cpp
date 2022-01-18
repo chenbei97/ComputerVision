1bee05564cc5fe05e39ab276a00bebe65625bb1f
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2022-01-18 16:51:31 +0800

    Add files via upload

---
+++ 数据结构与算法/C++语言/main.cpp
@@ -0,0 +1,79 @@
+#include "charter1.h"
+#include "charter2.h"
+#include "charter3.h"
+#include "charter4.h"
+#include "charter5.h"
+#include "charter6.h"
+#include "charter7.h"
+#include "charter8.h"
+#include "sort_method.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	//_charter1_throwError();
+	//_charter2_test_and_debug();
+	//_charter2_spatial_complexity();
+	//_charter2_time_complexity();
+	//_charter2_best_worst_average_operation_count();
+	//_charter2_count_sort_situ_rearrangement();
+	//_charter2_select_sort_timely_termination();
+	//_charter2_bubble_sort_timely_termination();
+	//_charter2_insert_sort();
+	//_charter2_calculation_of_program_steps();
+	//_charter2_analytic_method_steps();
+	//_charter2_analytic_best_worst_mean_steps();
+	//_charter3_binarySearch();
+	//_charter4_program_performance_measurement();
+	//_charter5_changeLength1D();
+	//_charter5_concrete_class_instantiation();
+	//_charter5_bidirectional_iterator();
+	//_charter5_arrayList_with_bidirectionalIterator();
+	//_charter5_linearList_by_vector();
+	//_charter6_chainNode_testCode();
+	//_charter6_chainList_testCode();
+	//_charter6_chainListWithIterator_testCode();
+	//_charter6_chainListWithIteratorExtend_testCode();
+	//_charter6_chainListCircular_testCode();
+	//_charter6_StudentRecord1_binSort_testCode();
+	//_charter6_StudentRecord2_binSort_testCode();
+	//_charter6_convex_hull();
+	//_charter7_array2D_pointer();
+	//_charter7_irregular_array2d();
+	//_charter7_matrixAdd();
+	//_charter7_matrixMultiply_sameSize();
+	// _charter7_matrixMultiply_differentSize();
+	//_charter7_matrixTranspose();
+	//_charter7_matrix_testCode();
+	 //_charter7_diagonalMatrix();
+	 //_charter7_tridiagonalMatrixRowMapping();
+	 //_charter7_tridiagonalMatrixColMapping();
+	 //_charter7_tridiagonalMatrixDiagMapping();
+	 //_charter7_lowerTriangularMatrixRow();
+	 //_charter7_lowerTriangularMatrixCol();
+	 //_charter7_upperTriangularMatrixRow();
+	 //_charter7_upperTriangularMatrixCol();
+	// _charter7_symmetric_matrix();
+	 //_charter7_sparse_matrix();
+	 //_charter7_linkedMatrix();
+	//_charter8_derivedArrayStack();
+	//_charter8_arrayStack();
+	//_charter8_linkedStack();
+	//_charter8_parenthesis_matching();
+	//_charter8_towerOfHanoi_byRecursion();
+	//_charter8_towerOfHanoi_byStack();
+	//_charter8_carriageRearrange();
+	_charter8_switchBoxWiring();
+	//const int n = 10;
+	//int a[n] = { 6,5,8,4,3,1,13,0,66 ,-2 };
+	//array_sorting_method(a, n ,countSort);
+	//std::cout << std::endl;
+	//array_sorting_method(a, n, selectSort, true);
+	//std::cout << std::endl;
+	//array_sorting_method(a, n, bubbleSort);
+	//std::cout << std::endl;
+	//array_sorting_method(a, n, insertSort,true);
+	//std::cout << std::endl;
+	system("pause");
+	return 0;
+}
