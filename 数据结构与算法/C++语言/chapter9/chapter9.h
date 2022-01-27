7869c602cf7bf71df640aaaac5ed303fcddf08a8
chenbei97 <66169263+chenbei97@users.noreply.github.com>
2022-01-27 11:46:46 +0800

    Update chapter9.h

--- 数据结构与算法/C++语言/chapter9/chapter9.h
+++ 数据结构与算法/C++语言/chapter9/chapter9.h
@@ -5,4 +5,7 @@ void _chapter9_arrayQueue();
 void _chapter9_linkedQueue();
 void _chapter9_carriageRearrange();
 void _chapter9_carriageRearrange_noQueue();
+void _chapter9_circuit_wiring();
+void _chapter9_element_recognition();
+void _chapter9_factory_simulation();
 #endif // !chapter9_
